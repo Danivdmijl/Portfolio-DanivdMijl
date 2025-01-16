60443dc00f1a4a31e5df41db42080afe4ff45a8a
Dani <90690863+Danivdmijl@users.noreply.github.com>
2025-01-16 12:14:27 +0100

    Added header + home page

---
+++ JS/scripts.js
@@ -0,0 +1,6 @@
+console.log("test voor de JS file");
+
+document.getElementById('hamburger-menu').addEventListener('click', function() {
+    document.getElementById('header_opties_container').classList.toggle('active');
+});
+
