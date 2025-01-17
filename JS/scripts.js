8d706bf977ec2a2f60c429b7e301308270f57455
Dani <90690863+Danivdmijl@users.noreply.github.com>
2025-01-17 12:22:23 +0100

    Added responsive header

--- JS/scripts.js
+++ JS/scripts.js
@@ -4,3 +4,6 @@ document.getElementById('hamburger-menu').addEventListener('click', function() {
     document.getElementById('header_opties_container').classList.toggle('active');
 });
 
+document.querySelector('.hamburger-menu').addEventListener('click', function() {
+  document.querySelector('header').classList.toggle('menu-open');
+});
