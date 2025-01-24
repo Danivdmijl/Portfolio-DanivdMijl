5703610a797d3cdb7cf0ee2fcc0b05656b58d70c
Dani <90690863+Danivdmijl@users.noreply.github.com>
2025-01-24 14:03:47 +0100

    Added contact form submit fix

--- JS/scripts.js
+++ JS/scripts.js
@@ -7,3 +7,25 @@ document.getElementById('hamburger-menu').addEventListener('click', function() {
 document.querySelector('.hamburger-menu').addEventListener('click', function() {
   document.querySelector('header').classList.toggle('menu-open');
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.querySelector(".contactformulier");
+  const submitButton = document.querySelector(".contactformulier_button");
+  const loadingAnimationContainer = document.getElementById("loading-animation");
+
+  // Lottie-animatie initialiseren
+  const loadingAnimation = lottie.loadAnimation({
+      container: loadingAnimationContainer, // de container waarin de animatie wordt geladen
+      renderer: "svg",
+      loop: true,
+      autoplay: false, // start pas wanneer het formulier wordt ingediend
+      path: "json/loading.json", // URL van de animatie
+  });
+
+  form.addEventListener("submit", function (e) {
+      // Zorg ervoor dat de animatie pas na submit verschijnt
+      submitButton.style.display = "none"; // Verberg de knop
+      loadingAnimationContainer.style.display = "block"; // Toon de animatie
+      loadingAnimation.play(); // Start de animatie
+  });
+});
